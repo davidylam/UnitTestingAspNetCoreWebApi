39099503ff6a271511de3bc279880b2edd47016d
David Lam <David.Lam@DecisionResearch.com>
2024-07-01 10:06:48 -1000

    Completed Module 3. Tackling Basic Unit Testing Scenarios

---
+++ Dev/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,64 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeFactoryTests
+    {
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("David", "Lam");
+
+            Assert.Equal(2500, employee.Salary);
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("David", "Lam");
+
+            Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salary not in acceptable range.");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_AlternativeWithInRange()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("David", "Lam");
+
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMustBeBetween2500And3500_PrecisionExample()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("David", "Lam");
+            employee.Salary = 2500.123m;
+
+            Assert.Equal(2500, employee.Salary, 0 );
+        }
+
+        [Fact]
+        public void CreateEmployee_IsExternalIsTrue_ReturnTypeMustBeExternalEmployee()
+        {
+            var employeeFactory = new EmployeeFactory();
+
+            var employee = employeeFactory.CreateEmployee("David", "Lam", "DRC Inc", true);
+
+            Assert.IsType<ExternalEmployee>(employee);
+        }
+
+    }
+}
---
+++ Dev/EmployeeManagement.Test/EmployeeTests.cs
@@ -0,0 +1,73 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameIsConcatenation()
+        {
+            var employee = new InternalEmployee("David", "Lam", 0, 2500, false, 1);
+
+            employee.FirstName = "Lucia";
+            employee.LastName = "SHELTON";
+
+            Assert.Equal("Lucia Shelton", employee.FullName, ignoreCase: true);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameStartsWithFirstName()
+        {
+            var employee = new InternalEmployee("David", "Lam", 0, 2500, false, 1);
+
+            employee.FirstName = "Lucia";
+            employee.LastName = "SHELTON";
+
+            Assert.StartsWith(employee.FirstName, employee.FullName);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameEndsWithLastName()
+        {
+            var employee = new InternalEmployee("David", "Lam", 0, 2500, false, 1);
+
+            employee.FirstName = "Lucia";
+            employee.LastName = "SHELTON";
+
+            Assert.EndsWith(employee.LastName, employee.FullName);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameContainsPartOfConcatenation()
+        {
+            // Arrange
+            var employee = new InternalEmployee("Kevin", "Dockx", 0, 2500, false, 1);
+
+            // Act
+            employee.FirstName = "Lucia";
+            employee.LastName = "Shelton";
+
+            // Assert
+            Assert.Contains("ia Sh", employee.FullName);
+        }
+
+        [Fact]
+        public void EmployeeFullNamePropertyGetter_InputFirstNameAndLastName_FullNameSoundsLikeConcatenation()
+        {
+            // Arrange
+            var employee = new InternalEmployee("Kevin", "Dockx", 0, 2500, false, 1);
+
+            // Act
+            employee.FirstName = "Lucia";
+            employee.LastName = "Shelton";
+
+            // Assert
+            Assert.Matches("Lu(c|s|z)ia Shel(t|d)on", employee.FullName);
+        }
+    }
+}
